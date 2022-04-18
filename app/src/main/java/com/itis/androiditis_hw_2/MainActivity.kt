b3a8937de9e0e7b9ec78e9673c6d529bcb21f714
OlgaMartazova <olyamarta34@gmail.com>
2022-04-18 10:48:24 +0300

    activity as container

---
+++ app/src/main/java/com/itis/androiditis_hw_2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.itis.androiditis_hw_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var controller: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        controller =
+            (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController
+    }
+}
